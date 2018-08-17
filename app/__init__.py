98033514b50ea4263b650190c83cd6954a79b792
Matt Swiszcz <mswis731@gmail.com>
2018-08-16 23:04:50 -0500

    Add support for Celery

--- app/__init__.py
+++ app/__init__.py
@@ -2,10 +2,16 @@ import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from celery import Celery
 
 app = Flask(__name__)
 app.config.from_object(os.environ["APP_SETTINGS"])
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+celery = Celery(app.name)
+celery.conf.update(
+    BROKER_URL=app.config["BROKER_URL"],
+    CELERY_RESULT_BACKEND=app.config["CELERY_RESULT_BACKEND"]
+)
 
-from app import views, models
+from app import views, models, tasks
--- config.py
+++ config.py
@@ -5,6 +5,8 @@ class Config(object):
     SECRET_KEY = os.environ["APP_SECRET_KEY"]
     SQLALCHEMY_DATABASE_URI = os.environ["DATABASE_URL"]
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    BROKER_URL = os.environ["REDIS_URL"]
+    CELERY_RESULT_BACKEND = os.environ["REDIS_URL"]
 
 class ProductionConfig(Config):
     DEBUG = True
