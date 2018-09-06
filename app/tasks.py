8665c99d36a225e4c9c89ab854c316f4ce46ce81
Matt Swiszcz <mswis731@gmail.com>
2018-09-06 11:21:42 -0500

    Add more Chrome options for Selenium

--- app/tasks.py
+++ app/tasks.py
@@ -16,6 +16,8 @@ def get_driver():
     chrome_options.binary_location = GOOGLE_CHROME_BIN
     chrome_options.add_argument("headless")
     chrome_options.add_argument("window-size=1200x600")
+    chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument('--disable-dev-shm-usage')
     driver = webdriver.Chrome(chrome_options=chrome_options)
     driver.implicitly_wait(10)
     return driver
